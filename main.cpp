25c8f8679e61646e7247b9117c70b29326f1a911
MAhmed <MAhmed@luxoft.com>
2022-11-30 19:52:55 +0100

    added code for the  + and += operators

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-// Nour Ahmed
+﻿// Nour Ahmed
 // Matrikal-Nr.: 5200991
 // Assignment 1 – Own string Class
 
@@ -23,171 +23,182 @@
 #include <iostream>
 
 int main() {
-    char charArray[] = "text in a const char array";
-    std::string stdString("another text in a std::string");
+	char charArray[] = "text in a const char array";
+	std::string stdString("another text in a std::string");
 
-    // instantiate objects
-    util::string string1;
-    util::string string2("initializing with const char array");
-    util::string string3(charArray);
-    util::string string4(stdString);
-    util::string string5(string4);
+	// instantiate objects
+	util::string string1;
+	util::string string2("initializing with const char array");
+	util::string string3(charArray);
+	util::string string4(stdString);
+	util::string string5(string4);
 
-    // header -----------------------------------------------------------------
+	// header -----------------------------------------------------------------
   /*  for (int k = 1; k < 255; k++)
-    {
-        std::cout << "\n";
-        std::cout << k << "\x1B[" << k << "m  Texting\033[0m\t\t";
-    }*/
+	{
+		std::cout << "\n";
+		std::cout << k << "\x1B[" << k << "m  Texting\033[0m\t\t";
+	}*/
 
 
-    util::printHeader("NourUtilString Application");
-    std::cout << "- Nour Ahmed                                                     -" << std::endl;
-    std::cout << "- Matrikal-Nr.: 5200991                                          -" << std::endl;
-    std::cout << "- Assignment 1 - Own string Class                                -" << std::endl;
-    std::cout << "-------------------------------------------------------------------------------\n\n";
+	util::printHeader("NourUtilString Application");
+	std::cout << "- Nour Ahmed                                                                  -" << std::endl;
+	std::cout << "- Matrikal-Nr.: 5200991                                                       -" << std::endl;
+	std::cout << "- Assignment 1 - Own string Class                                             -" << std::endl;
+	std::cout << "-------------------------------------------------------------------------------\n\n";
 
-    // Test Object Instantiation ----------------------------------------------
-    util::printSubHeader("Variable used for testing and their values");
-    std::cout << "Variable used for testing and their values" << std::endl;
+	// Test Object Instantiation ----------------------------------------------
+	util::printSubHeader("Variable used for testing and their values");
+	std::cout << "Variable used for testing and their values" << std::endl;
 
-    std::cout << " > [charArray]  charArray (size: " << util::string::rawSize(charArray) << ")  : " << charArray << std::endl;
-    std::cout << " > [stdString]  charArray (size: " << stdString.length() << ")  : " << stdString << std::endl;
+	std::cout << " > [charArray]  charArray (size: " << util::string::rawSize(charArray) << ")  : " << charArray << std::endl;
+	std::cout << " > [stdString]  charArray (size: " << stdString.length() << ")  : " << stdString << std::endl;
 
-    std::cout << "-------------------------------------------------------------------------------\n\n";
+	std::cout << "-------------------------------------------------------------------------------\n\n";
 
-    util::printSubHeader("Test object constructors and initialization");
-    std::cout << " > [test with default constructors]  string1 (size: " << string1.size() << ")  : " << string1 << std::endl;
-    std::cout << " > [test with const char*]           string2 (size: " << string2.size() << ") : " << string2 << std::endl;
-    std::cout << " > [test with std::string]           string3 (size: " << string3.size() << ") : " << string3 << std::endl;
-    std::cout << " > [test with char array]            string4 (size: " << string4.size() << ") : " << string4 << std::endl;
-    std::cout << " > [test with util::string]          string5 (size: " << string5.size() << ") : " << string4 << std::endl;
-    std::cout << "-------------------------------------------------------------------------------\n\n";
-
-
-    // Test member methods ---------------------------------------------------------
-    util::printSubHeader("Test Member Methods");
-    util::string temp = string2.substr(3, 5);
-    std::cout << " > [test substr()]           string2 (size: " << string2.size() << ") : " << string2 << std::endl;
-    std::cout << " > [test substr()]           substr(3,5) (size: " << temp.size() << ") : " << temp << std::endl;
+	util::printSubHeader("Test object constructors and initialization");
+	std::cout << " > [test with default constructors]  string1 (size: " << string1.size() << ")  : " << string1 << std::endl;
+	std::cout << " > [test with const char*]           string2 (size: " << string2.size() << ") : " << string2 << std::endl;
+	std::cout << " > [test with std::string]           string3 (size: " << string3.size() << ") : " << string3 << std::endl;
+	std::cout << " > [test with char array]            string4 (size: " << string4.size() << ") : " << string4 << std::endl;
+	std::cout << " > [test with util::string]          string5 (size: " << string5.size() << ") : " << string4 << std::endl;
+	std::cout << "-------------------------------------------------------------------------------\n\n";
 
 
-
-    std::cout << "-------------------------------------------------------------------------------\n\n";
-
-    // Test operators ---------------------------------------------------------
-    util::printSubHeader("Test operators");
-
-    string1 = string2;
-    string2 = "more text for testing";
-    string3 = std::string("text for std::string assignment");
-
-    std::cout << "[test = operator util::string = util::string]\n\t string1 = string2 -> string1 (size: " << string1.size() << "): " << string1 << std::endl;
-    std::cout << "[test = operator util::string = const char*]\n\t string2 = \"...\" -> string2 (size: " << string2.size() << "): " << string2 << std::endl;
-    std::cout << "[test = operator util::string = std::string]\n\t string3 = std::string(\"...\") -> string3 (size: " << string3.size() << "): " << string3 << std::endl;
-
-    std::cout << "-------------------------------------------------------------------------------\n\n";
-
-    string1 = string2;
-    std::cout << "[test == operator util::string == util::string]\n\t string1 == string2   -> " << ((string1 == string2)?"true":"false") << std::endl;
-
-    std::cout << "[test == operator util::string == std::string]\n\t string1 == stdString -> " << ((string1 == stdString) ? "true" : "false") << std::endl;
-    std::cout << "[test == operator std::string == util::string]\n\t stdString == string1 -> " << ((stdString == string1) ? "true" : "false") << std::endl;
-
-    std::cout << "[test == operator util::string == const char*]\n\t string1 == charArray -> " << ((string1 == charArray) ? "true" : "false") << std::endl;
-    std::cout << "[test == operator const char* == util::string]\n\t charArray == string1 -> " << ((charArray == string1) ? "true" : "false") << std::endl;
-
-    std::cout << "-------------------------------------------------------------------------------\n\n";
-
-    std::cout << "[test != operator util::string != util::string]\n\t string1 != string2   -> " << ((string1 != string2) ? "true" : "false") << std::endl;
-
-    std::cout << "[test != operator util::string != std::string]\n\t string1 != stdString -> " << ((string1 != stdString) ? "true" : "false") << std::endl;
-    std::cout << "[test != operator std::string != util::string]\n\t stdString != string1 -> " << ((stdString != string1) ? "true" : "false") << std::endl;
-
-    std::cout << "[test != operator util::string != const char*]\n\t string1 != charArray -> " << ((string1 != charArray) ? "true" : "false") << std::endl;
-    std::cout << "[test != operator const char* != util::string]\n\t charArray != string1 -> " << ((charArray != string1) ? "true" : "false") << std::endl;
-    std::cout << "-------------------------------------------------------------------------------\n\n";
-
-    std::cout << "[test [] operator util::string[]]" << std::endl;
-    std::cout << "\tstring1: " << string1 << "-> string1[0]: " << string1[0]<< std::endl;
-    std::cout << "\tstring2: " << string2 << "-> string2[3]: " << string2[3] << std::endl;
-    std::cout << "\tstring3: " << string3 << "-> string3[50]: " << string3[50] << std::endl;
-    std::cout << "-------------------------------------------------------------------------------\n\n";
-
-    //std::cout << " > [util::string + const char*]  string4 + charArray: " << string4 + charArray << std::endl;
-    string2= string2 + string3;
-    std::cout << "string2 (size: " << string2.size() << ") : " << string2 << std::endl;
-    std::cout << "string3 (size: " << string3.size() << ") : " << string3 << std::endl;
-    std::cout << " > [util::string + util::string]  string2 + string3: " << string2 + string3
-         << "\nstring2 (size: " << string2.size() << ") : " << string2 << std::endl;
-
-
-    std::cout << "-------------------------------------------------------------------------------\n\n";
-
-    std::cout << "[test << operator std::cout << util::string << int << std::string << char *]:\n\t"
-        << string1 << ", "
-        << "size: "
-        << string1.size() << ", "
-        << stdString << ", "
-        << charArray
-        << std::endl;
+	// Test member methods ---------------------------------------------------------
+	util::printSubHeader("Test Member Methods");
+	util::string temp = string2.substr(3, 5);
+	std::cout << " > [test substr()]           string2 (size: " << string2.size() << ") : " << string2 << std::endl;
+	std::cout << " > [test substr()]           substr(3,5) (size: " << temp.size() << ") : " << temp << std::endl;
 
 
 
-    //std::cout << " > [util::string + const char*]  string4 + charArray: " << string4 + charArray << std::endl;
+	std::cout << "-------------------------------------------------------------------------------\n\n";
 
-    //util::string string6(string4 + charArray);
+	// Test operators ---------------------------------------------------------
+	util::printSubHeader("Test operators");
 
-    //std::cout << " > [test with util::string]          string6 (size: " << string6.size() << ") : " << string6 << std::endl;
+	std::cout << "[test << operator std::cout << util::string << int << std::string << char *]:\n\t"
+		<< string2 << ", "
+		<< "size: "
+		<< string2.size() << ", "
+		<< stdString << ", "
+		<< charArray
+		<< std::endl;
+	std::cout << "-------------------------------------------------------------------------------\n\n";
 
-    std::cout << "-------------------------------------------------------------------------------\n\n";
+	//string2 = string2 + string3;
+	std::cout << "> [test + operator util::string + util::string] \n  string2 + string3 -> " << string2 + string3 << std::endl;
 
-   //std::cout << " > [util::string + const char*]: " << util::concat(string4.c_str(), charArray) << std::endl;
+	string5 = string5 + " see how + operator with char * works";
+	std::cout << "> [test + operator util::string + const char*] \n  string5 = string5 + const char* -> (size: "
+		<< string5.size() << ") : " << string5;
+	std::cout << "\n-------------------------------------------------------------------------------\n\n";
 
-    std::cout << "-------------------------------------------------------------------------------\n\n";
+	string4 += string3;
+	std::cout << "> [test += operator with util::string] \n  string4 += string3 -> (size: "
+		<< string4.size() << ") : " << string4 << std::endl;
 
-    char s1[100] = "programming ", s2[] = "is awesome";
-    std::cout << "s1: " << s1 << std::endl;
-    std::cout << "s2: " << s2 << std::endl;
-
-    /*util::concat(s1, s2, 2);
-    std::cout << "s1: " << s1 << std::endl;
-
-    util::concat(s1, s2);
-    std::cout << "s1: " << s1 << std::endl;
-
-    util::deepCopy(s1, s2, 3 , 10);
-    std::cout << "s1: " << s1 << std::endl;*/
-
-    util::deepCopy(s1, s2);
-    std::cout << "s1: " << s1 << std::endl;
-
-
- /*
-    delete[] a;
-    delete[] b;*/
-
-    //MyString::String string1;
-    //MyString::String string2("Welt");
+	string4 += " here += operator is used to add more text in char *";
+	std::cout << "> [test += operator with const char*] \n  string4 += const char* -> (size: "
+		<< string4.size() << ") : " << string4 << std::endl;
+	std::cout << "-------------------------------------------------------------------------------\n\n";
 
 
-    //string1.setText("Hallo ");
-    //////string2.add("!");
+	string1 = string2;
+	string2 = "more text for testing";
+	string3 = std::string("text for std::string assignment");
 
-    ////MyString::String pstring = string1 + string2 + " with character array added";
+	std::cout << "[test = operator util::string = util::string]\n\t string1 = string2 -> string1 (size: " << string1.size() << "): " << string1 << std::endl;
+	std::cout << "[test = operator util::string = const char*]\n\t string2 = \"...\" -> string2 (size: " << string2.size() << "): " << string2 << std::endl;
+	std::cout << "[test = operator util::string = std::string]\n\t string3 = std::string(\"...\") -> string3 (size: " << string3.size() << "): " << string3 << std::endl;
 
-    //std::cout << "String1 and String2: " << string1 << string2 << std::endl;
-    //std::cout << "String1 + String2: " << pstring << std::endl;
-    //std::cout << "Comparing 1 with 2: " << (string1 == string2) << std::endl;
-    //std::cout << "Comparing 1 with 1: " << (string1 == string1) << std::endl;
-    //std::cout << "Get: " << string1[0] << std::endl;
+	std::cout << "-------------------------------------------------------------------------------\n\n";
 
-    //MyString::It<char> it = pstring.begin();
-    //while (it != pstring.end()) {
-    //    std::cout << *it << std::endl;
-    //    ++it;
-    //}
+	string1 = string2;
+	std::cout << "[test == operator util::string == util::string]\n\t string1 == string2   -> " << ((string1 == string2) ? "true" : "false") << std::endl;
+
+	std::cout << "[test == operator util::string == std::string]\n\t string1 == stdString -> " << ((string1 == stdString) ? "true" : "false") << std::endl;
+	std::cout << "[test == operator std::string == util::string]\n\t stdString == string1 -> " << ((stdString == string1) ? "true" : "false") << std::endl;
+
+	std::cout << "[test == operator util::string == const char*]\n\t string1 == charArray -> " << ((string1 == charArray) ? "true" : "false") << std::endl;
+	std::cout << "[test == operator const char* == util::string]\n\t charArray == string1 -> " << ((charArray == string1) ? "true" : "false") << std::endl;
+
+	std::cout << "-------------------------------------------------------------------------------\n\n";
+
+	std::cout << "[test != operator util::string != util::string]\n\t string1 != string2   -> " << ((string1 != string2) ? "true" : "false") << std::endl;
+
+	std::cout << "[test != operator util::string != std::string]\n\t string1 != stdString -> " << ((string1 != stdString) ? "true" : "false") << std::endl;
+	std::cout << "[test != operator std::string != util::string]\n\t stdString != string1 -> " << ((stdString != string1) ? "true" : "false") << std::endl;
+
+	std::cout << "[test != operator util::string != const char*]\n\t string1 != charArray -> " << ((string1 != charArray) ? "true" : "false") << std::endl;
+	std::cout << "[test != operator const char* != util::string]\n\t charArray != string1 -> " << ((charArray != string1) ? "true" : "false") << std::endl;
+	std::cout << "-------------------------------------------------------------------------------\n\n";
+
+	std::cout << "[test [] operator util::string[]]" << std::endl;
+	std::cout << "\tstring1: " << string1 << "-> string1[0]: " << string1[0] << std::endl;
+	std::cout << "\tstring2: " << string2 << "-> string2[3]: " << string2[3] << std::endl;
+	std::cout << "\tstring3: " << string3 << "-> string3[50]: " << string3[50] << std::endl;
+	std::cout << "-------------------------------------------------------------------------------\n\n";
+
+	
 
 
-    return 0;
+
+	//std::cout << " > [util::string + const char*]  string4 + charArray: " << string4 + charArray << std::endl;
+
+	//util::string string6(string4 + charArray);
+
+	//std::cout << " > [test with util::string]          string6 (size: " << string6.size() << ") : " << string6 << std::endl;
+
+	std::cout << "-------------------------------------------------------------------------------\n\n";
+
+	//std::cout << " > [util::string + const char*]: " << util::concat(string4.c_str(), charArray) << std::endl;
+
+	std::cout << "-------------------------------------------------------------------------------\n\n";
+
+	char s1[100] = "programming ", s2[] = "is awesome";
+	std::cout << "s1: " << s1 << std::endl;
+	std::cout << "s2: " << s2 << std::endl;
+
+	/*util::concat(s1, s2, 2);
+	std::cout << "s1: " << s1 << std::endl;
+
+	util::concat(s1, s2);
+	std::cout << "s1: " << s1 << std::endl;
+
+	util::deepCopy(s1, s2, 3 , 10);
+	std::cout << "s1: " << s1 << std::endl;*/
+
+	util::deepCopy(s1, s2);
+	std::cout << "s1: " << s1 << std::endl;
+
+
+	/*
+	   delete[] a;
+	   delete[] b;*/
+
+	   //MyString::String string1;
+	   //MyString::String string2("Welt");
+
+
+	   //string1.setText("Hallo ");
+	   //////string2.add("!");
+
+	   ////MyString::String pstring = string1 + string2 + " with character array added";
+
+	   //std::cout << "String1 and String2: " << string1 << string2 << std::endl;
+	   //std::cout << "String1 + String2: " << pstring << std::endl;
+	   //std::cout << "Comparing 1 with 2: " << (string1 == string2) << std::endl;
+	   //std::cout << "Comparing 1 with 1: " << (string1 == string1) << std::endl;
+	   //std::cout << "Get: " << string1[0] << std::endl;
+
+	   //MyString::It<char> it = pstring.begin();
+	   //while (it != pstring.end()) {
+	   //    std::cout << *it << std::endl;
+	   //    ++it;
+	   //}
+
+
+	return 0;
 }
--- utilstring.h
+++ utilstring.h
@@ -17,6 +17,11 @@
  * @createdOn      :  23.11.2022
  *========================================================================**/
 
+ // see: https://gist.github.com/philipheimboeck/099e540d800063e3e6ec
+ // see:
+ // https://codereview.stackexchange.com/questions/98329/stdstring-implementation
+ // see: https://en.wikipedia.org/wiki/Snake_case
+
 #ifndef UTILSTRING_H
 #define UTILSTRING_H
 
@@ -25,113 +30,111 @@
 #include <string>
 
 namespace util {
-    // Initially, the class shall provide memory for 10 printable characters
-    #define INITIAL_SIZE 10
+// Initially, the class shall provide memory for 10 printable characters
+#define INITIAL_SIZE 10
 
-    class string {
-        char* internal_buffer;
-        size_t buffer_size;
-        //size_t string_size;
+	class string {
+		char* internal_buffer;
+		size_t buffer_size;
+		// size_t string_size;
 
-       public:
-        /*================== Constructors ===============*/
-        string(size_t intialSize= INITIAL_SIZE);  /*!< default constructor with empty initialization */
-        string(const char*);        /*!< Constructor with parameter const char* */
-        string(const std::string&); /*!< Constructor with parameter std::string */
-        string(const string&);      /*!< Copy constructor: Creates a deep copy of a passed string */
+	public:
+		/*================== Constructors ===============*/
+		string(size_t intialSize = INITIAL_SIZE); /*!< default constructor with empty initialization */
+		string(const char*);        /*!< Constructor with parameter const char* */
+		string(const std::string&); /*!< Constructor with parameter std::string */
+		string(const string&); /*!< Copy constructor: Creates a deep copy of a passed string */
 
+		/*================== Destructor =================*/
+		~string(void);
 
-        /*================== Destructor =================*/
-        ~string(void);
+		/*================== Methods ====================*/
 
+		void intialize_string(size_t length = 0);
+		void deepCopy(const char* rawChar, size_t startPosition = 0);
+		// void concat(char* rawCharTarget, 
+		//             const char* rawCharSource, 
+		//             size_t startPosition = 0);
 
-        /*================== Methods ====================*/
+		// Returns a pointer to an array that contains a null-terminated
+		// sequence of characters(i.e., a C-string) representing the current
+		// value of the string object.
+		// Returns a substring object of type util::string which
+		// starts at pos. Parameter length specifies the amount of
+		// characters of the new util::string to be returned.
+		string substr(size_t pos, size_t length);
 
-        void intialize_string(size_t length = 0);
-        void deepCopy(const char* rawChar, size_t startPosition = 0);
-        //void concat(char* rawCharTarget, const char* rawCharSource, size_t startPosition = 0);
+		// Allows raw access to the internal C-string (through its char* pointer)
+		char* c_str() const;
 
+		bool compare(const char* charArray) const;
+		static int compare(const char* s1, const char* s2);
 
+		// Clears your string object
+		// Erases the contents of the string, which becomes an empty string(with
+		// a length of 0 characters).
+		void clear();
 
-        // Returns a pointer to an array that contains a null-terminated
-        // sequence of characters(i.e., a C-string) representing the current
-        // value of the string object.
-        // Returns a substring object of type util::string which
-        // starts at pos. Parameter length specifies the amount of
-        // characters of the new util::string to be returned.
-        string substr(size_t pos, size_t length);
+		size_t size() const; /*!< Get the length of the string */
+		static size_t rawSize(const char* rawChar);
 
-        // Allows raw access to the internal C-string (through its char* pointer)
-        char* c_str() const;
+		// Returns the amount of characters of your string excluding \0.
+		// Might be smaller than the actual reserved memory.
+		size_t length() const;
 
-        bool compare(const char* charArray) const;
-        static int compare(const char* s1, const char* s2);
+		/*================== Operators ====================*/
+		string operator+(const string& rhsString);
+		string operator+(const char* strInstance);
 
-        // Clears your string object
-        // Erases the contents of the string, which becomes an empty string(with
-        // a length of 0 characters).
-        void clear();
+		string& operator+=(const string& rhsString);
+		string& operator+=(const char* strInstance);
 
+		string& operator=(const string& rhsString);
+		string& operator=(const char* rhsCharArray);
+		string& operator=(const std::string& rhsString);
 
-        size_t size() const;                        /*!< Get the length of the string */
-        static size_t rawSize(const char* rawChar);
+		bool operator==(const string& rhsString);
+		bool operator==(const std::string& rhsString);
+		bool operator==(const char* charArray);
 
-        // Returns the amount of characters of your string excluding \0.
-        // Might be smaller than the actual reserved memory.
-        size_t length() const;
+		bool operator!=(const string& rhsString);
+		bool operator!=(const std::string& rhsString);
+		bool operator!=(const char* charArray);
 
+		const char operator[](size_t position);
 
-        /*================== Operators ====================*/
-        string& operator+(const string& rhsString);
-        string& operator+(const char* strInstance);
+		/*=========================================================================*
+		 *             Non-member function overloads                               *
+		 *=========================================================================*/
+		 // Free operator methods for the cases util::string is on the rhs
+		 // Friendship enables access to private members
+		friend std::ostream& operator<<(std::ostream& iostream,
+			const util::string& myString);
 
+		friend bool operator==(const std::string& lhsString,
+			const util::string& rhsString);
+		friend bool operator==(const char* lhsCharArray,
+			const util::string& rhsString);
 
-        string& operator=(const string& rhsString);
-        string& operator=(const char* rhsCharArray);
-        string& operator=(const std::string& rhsString);
+		friend bool operator!=(const std::string& lhsString,
+			const util::string& rhsString);
+		friend bool operator!=(const char* lhsCharArray,
+			const util::string& rhsString);
+	};
 
-        bool operator==(const string& rhsString);
-        bool operator==(const std::string& rhsString);
-        bool operator==(const char* charArray);
+	/*=========================================================================*
+	 *             Some Utility functions                                      *
+	 *=========================================================================*/
+	void concat(char* rawCharTarget, char* rawCharSource,
+		size_t startPosition = -1);
 
-        bool operator!=(const string& rhsString);
-        bool operator!=(const std::string& rhsString);
-        bool operator!=(const char* charArray);
+	void deepCopy(char* rawCharTarget, const char* rawCharSource,
+		size_t destStartPosition = -1, size_t srcEndPosition = -1);
 
+	//-----------------------------------------------
+	void printHeader(const char* text);
+	void printSubHeader(const char* text);
 
-        const char operator[](size_t position);
-
-
-        /*=========================================================================*
-         *             Non-member function overloads                               *
-         *=========================================================================*/
-         // Free operator methods for the cases util::string is on the rhs
-         // Friendship enables access to private members
-        friend std::ostream& operator<<(std::ostream& iostream, const util::string& myString);
-
-        friend bool operator==(const std::string& lhsString, const util::string& rhsString);
-        friend bool operator==(const char* lhsCharArray, const util::string& rhsString);
-
-        friend bool operator!=(const std::string& lhsString, const util::string& rhsString);
-        friend bool operator!=(const char* lhsCharArray, const util::string& rhsString);
-    };
-
-    /*=========================================================================*
-     *             Some Utility functions                                      *
-     *=========================================================================*/
-    void concat(char* rawCharTarget,  char* rawCharSource, size_t startPosition = -1);
-
-    void deepCopy(char* rawCharTarget, const char* rawCharSource, size_t destStartPosition = -1, size_t srcEndPosition = -1);
-
-
-
-//-----------------------------------------------
-    void printHeader(const char* text);
-    void printSubHeader(const char* text);
-
-
-
-}  // namespace util
-
+} // namespace util
 
 #endif /* UTILSTRING_H */
